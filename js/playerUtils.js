e7eef2c65d134be56affc7eacc8137be06f7b2c3
Munz-ctrl <77phoenixlane@gmail.com>
2025-06-03 10:00:27 -0400

    Update playerUtils.js

--- js/playerUtils.js
+++ js/playerUtils.js
@@ -30,18 +30,18 @@ function createPlayerMarker(player) {
   const marker = L.marker(player.coords, { icon: iconWithPing }).addTo(map); // ✅ save it into "marker"
 
  marker.on('click', () => {
-  
+   closeAllPopups();
 
-  // Disable clicks on the selected player's marker
-  const markerEl = marker.getElement();
-  if (markerEl) markerEl.style.pointerEvents = 'none';
+   // Disable clicks on the selected player's marker
+   const markerEl = marker.getElement();
+   if (markerEl) markerEl.style.pointerEvents = 'none';
 
   // Re-enable after popup closes
   marker.on('popupclose', () => {
     if (markerEl) markerEl.style.pointerEvents = 'auto';
   });
 
-  closeAllPopups();
+ 
 
 });
 
