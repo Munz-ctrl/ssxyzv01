c4ac7d99ca3dcfa0da00b62f7739e2074e151e71
Munz-ctrl <77phoenixlane@gmail.com>
2025-12-25 18:27:16 -0500

    Update dressup.js

--- apps/tools/dressup/js/dressup.js
+++ apps/tools/dressup/js/dressup.js
@@ -297,7 +297,7 @@ if (qsId) {
 if (qsSkin) {
   currentSkinName = qsSkin;
 } else {
-  currentSkinName = "@isoMunzir";
+  currentSkinName = "Munzir";
 }
 
 // Choose hero image: ?hero=... wins, otherwise default
